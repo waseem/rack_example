a7024d97dab2865763bbfd79e24005214a954350
Waseem Ahmad <talk.to.waseem@gmail.com>
2011-04-17 23:33:32 +0530

    No IE Rack example.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RackExample::Application.config.secret_token = 'e7ddf1d3fafa21273e5f68b23ada5cd3d58416318e1b411125e7b9c3a2b24fde6c876ca05486346d991cfb475539a05904c8d64250ec00a0e942b9e0ae47d8c8'
